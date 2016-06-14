dd7dc05d0ed69d759db499b625e7b7c3e7d933a7
Gisumwa Mairura <gisumwa@gmail.com>
2016-06-14 15:54:43 +0300

    Initial Fibonnacci series Commit

---
+++ Fibonnacci.py
@@ -0,0 +1,10 @@
+def fibonnacci(terms):
+    if terms==1:
+        return terms
+    else:
+        return fibonnacci(terms-1)+fibonnacci(terms-1)
+print(fibonnacci(3))
+beast="okay"
+print("Niaje {}".format(beast))
+print("Hellow",beast)
+
